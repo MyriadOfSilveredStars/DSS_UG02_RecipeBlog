a7e0cc2f94ba6a41aa87070d088fc248d6e6d4b4
jaygroomuni <dsf22kcu@uea.ac.uk>
2026-03-22 12:13:43 +0000

    Create db.js

---
+++ db.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+pool.connect()
+  .then(client => {
+    console.log('Connected to PostgreSQL');
+    client.release();
+  })
+  .catch(err => {
+    console.error('Connection error:', err);
+  });
+
+module.exports = pool;

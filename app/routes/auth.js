fdde322a826236650312f3dc2e03fdb94c9854d1
Riley <randymagic241@gmail.com>
2026-04-21 15:26:06 +0100

    apply rateLimiter backend

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,6 +3,7 @@ const { body } = require("express-validator");
 const { register } = require("../controllers/auth");
 const { login } = require("../controllers/auth");
 const authenticateJWT = require("../middlewares/authenticateJWT");
+const rateLimiter = require("../middlewares/loginLimit");
 const { dashboard } = require("../controllers/auth");
 const router = Router();
 
@@ -17,7 +18,7 @@ const validateLogin = [
     body("email").isEmail().notEmpty().withMessage("Please enter a valid email address.").trim().escape(),
     body("password").notEmpty().withMessage("Please enter a password.").trim().escape(),
 ]
-router.post("/login", validateLogin, login);
+router.post("/login", rateLimiter, validateLogin, login);
 
 router.get('/dashboard', authenticateJWT, dashboard);
 

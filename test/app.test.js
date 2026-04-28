7dc97514229c7caf467a4872f6c46ed608a69459
Rho Bates <rhobates@outlook.com~>
2026-04-28 17:56:58 +0100

    i am on the verge with this unit testing

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,12 @@
-const app = require("../app/app");
-const request = require("supertest")(app)
+//const app = require("../app/app");
+//const request = require("supertest")(app)
 const assert = require("assert");
 
+const authController = require('../app/controllers/auth');
+const register = authController.register;
 
-const registeruser = require("../app/controllers/auth").register;
-const loginuser = require("../app/controllers/auth").login;
-const dashboard = require("../app/controllers/auth").dashboard;
-
-const dashboard_request = require("supertest")(dashboard);
+const auth = require('../app/routes/auth');
+const request = require('supertest')(auth);
 
 //variables to use in testing
 var username = "johndoe";
@@ -27,8 +26,8 @@ const testdata = {
     };
 
 //let the testing begin
-
-describe('Check Connection', function() {
+/* 
+describe('Check Connection to App', function() {
 
     //this test works
     it("Connects to app.js", function(done) {
@@ -37,26 +36,21 @@ describe('Check Connection', function() {
             .expect(200, done);
     });
 
-    //this one fails, because it's connecting to a controller i think...
-    it("Connects to auth dashboard", function(done){
-        dashboard_request
-        .get('/')
-        .expect(200, done);
+});
+*/
+
+//I HAVE IT
+//i did not in fact have it
+// i can send data TO the auth.js controller, but not get status codes back
+// that's about it actually it doesn't run if i call it with supertest
+// my console uses bash because this coursework makes me want to bash my head against a rock
+
+describe('Test registering a new user', function() {
+    it ("Should return code 422 for missing information", async function() {
+
+        request
+        .get('/api/register')
+        .expect(422);
     })
-
-});
-
-//everything beyond this fails miserably
-//i need to test auth and pass values through :/
-describe("Signing Up", () => {
-    it("on the verge", async () => {
-        const res = await request(registeruser).get('/', testdata).expect(200);
-    });
-});
-
-describe("Dashboard", () => {
-    it("i am near death", async () => {
-        const res = await request(dashboard).get('/').expect(200);
-    });
 })
 
